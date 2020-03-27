05590af83ae79e37909882394f52dbb56287c42b
FjodBorg <Fjordormaster@hotmail.com>
2020-03-27 11:37:18 +0100

    Move function is now implemented

--- modules/actions.py
+++ modules/actions.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 from abc import ABC, ABCMeta, abstractmethod
+import operator
 
 
 class Literals(ABC):
@@ -8,8 +9,10 @@ class Literals(ABC):
         self.agents = {}  # hashtable
         self.goals = {}  # hashtable
         self.boxes = {}  # hashtable
-        
         super().__init__()
+
+    def addMap(self, map2):
+        self.map = np.array(map2)
 
     def addAgent(self, key, value):
         # key is color
@@ -35,22 +38,28 @@ class Literals(ABC):
             # if key already exists, make the value a list
             self.boxes[key].append(value)
 
-    def AgentAt(obj, pos):
+    def GetPos(self, objtype, obj):
+        if obj in objtype:
+            return objtype[obj]
+        else:
+            return None
+
+    def SetPos(self, objtype, obj, pos, i):
+        objtype[obj][i] = pos
+
+    def GaolAt(self, obj, pos):
         pass
 
-    def GaolAt(obj, pos):
+    def BoxAt(self, obj, pos):
         pass
 
-    def BoxAt(obj, pos):
+    def Free(self, pos):
+        return self.map[pos]
+
+    def Color(self, obj):
         pass
 
-    def Free(pos):
-        pass
-
-    def Color(obj):
-        pass
-
-    def Neighbour(pos1, pos2):
+    def Neighbour(self, pos1, pos2):
         pass
 
     @abstractmethod
@@ -69,35 +78,72 @@ class Literals(ABC):
 
 class StateInit(Literals):
     def __init__(self):
+        # it is (row, column) and not (x, y)
+        self.dir = {"N": (-1, 0), "E": (0, 1), "S": (1, 0), "W": (0, -1)}
         super().__init__()
 
-    def Move(self, agt, agtfrom, agtto):
-        Literals.Free(agtto)
-        Literals.Neighbour(agtfrom, agtto)
-        Literals.AgentAt(agt, agtfrom)
+    def AddPos(self, agtfrom, agtdir, i=0):
+        return tuple(map(operator.add, agtfrom[i], self.dir[agtdir]))
 
-    def Push(self, agt, agtfrom, box, boxfrom, boxto):
-        Literals.Free(boxto)
-        Literals.Neighbour(agtfrom, boxfrom)
-        Literals.Neighbour(boxfrom, boxto)
-        Literals.BoxAt(box, agtfrom)
-        Literals.AgentAt(agt, agtfrom)
+    def Move(self, agt, agtdir, i=0):
+        agtfrom = super().GetPos(self.agents, agt)
+        if agtfrom is None:
+            print("agent", agt, "does not exist")
+            return None
+        if agtdir not in self.dir:
+            print("Direction", agtdir, "does not exist")
+            return None
+        # TODO figure something out about having two ai's of the same type
+        # right now we assume one 1 ai of each type [look at variable i]
+        # if we can confirm that there only is one of each type, remove [i]
+        # in Agent, the line below and in the AddAgent method
+        # aka agtfrom[i] becomes agtfrom since agtfrom isn't a list anymore
+        agtto = self.AddPos(agtfrom, agtdir, i)
+        if super().Free(agtto) == chr(32):
+            super().SetPos(self.agents, agt, agtto, i)
+            return "Agent " + agt + " is now at " + str(agtto) + " (row,col)"
+        else:
+            return "Pos " + str(agtto) + " (row,col) is not free"
+
+    def Push(self, agt, box, boxdir):
+        super().Free(boxto)
+        super().Neighbour(agtfrom, boxfrom)
+        super().Neighbour(boxfrom, boxto)
+        super().BoxAt(box, agtfrom)
+        super().AgentAt(agt, agtfrom)
 
     def Pull(agt, agtfrom, agtto, box, boxfrom):
-        Literals.Free(agtto)
-        Literals.Neighbour(agtto, boxfrom)
-        Literals.Neighbour(boxfrom, agtfrom)
-        Literals.BoxAt(box, agtfrom)
-        Literals.AgentAt(agt, agtfrom)
+        super().Free(agtto)
+        super().Neighbour(agtto, boxfrom)
+        super().Neighbour(boxfrom, agtfrom)
+        super().BoxAt(box, agtfrom)
+        super().AgentAt(agt, agtfrom)
 
     def Noop(self, agt):
         pass
 
 
 def test():
+    # remember to make walls, otherwise it isn't bound to the matrix!
     state = StateInit()
-    state.Move(1, 2, 3)
+    state.addMap(
+        [
+            ["+", "+", "+", "+", "+"],
+            ["+", chr(32), chr(32), chr(32), "+"],
+            ["+", chr(32), chr(32), "+", "+"],
+            ["+", "+", chr(32), chr(32), "+"],
+            ["+", "+", "+", "+", "+"],
+        ]
+    )
+    print(state.map)
     state.addAgent("b", (1, 1))  # key is color
+    print(state.agents)
+    print(state.Move("b", "S"))
+    print(state.agents)
+    print(state.Move("b", "E"))
+    print(state.agents)
+    print(state.Move("b", "S"))
+    print(state.agents)
     state.addAgent("b", (1, 2))
     state.addAgent("b", (1, 2))
     state.addAgent("c", (1, 2))  # key is letter
@@ -108,17 +154,17 @@ def test():
     state.addBox(("c", "c"), (1, 1))
     state.addBox(("c", "b"), (5, 4))
 
-    print(state.agents)
+    '''print(state.agents)
     print(state.goals)
     print(state.boxes)
 
-    print(state.agents['b'])
+    print(state.agents["b"])'''
 
 
 # uncomment to see an example of the structure
 # test()
 
-# use 
+# use
 # from modules import actions
 # to load the module from root
 # then call actions.test() to run the test
