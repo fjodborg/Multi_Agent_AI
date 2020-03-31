73e82e7bc8aea01a008e7bea17e15121493a8dd4
FjodBorg <Fjordormaster@hotmail.com>
2020-03-31 14:37:48 +0200

    revamped action schemas, should work now, but will most likely explore already explored notes

--- multi_sokoban/actions.py
+++ multi_sokoban/actions.py
@@ -4,56 +4,63 @@ import operator
 import copy
 
 
-class Literals():
-    def __init__(self, parent: 'Literals' = None):
+class Literals:
+    def __init__(self, parent: "Literals" = None):
         if parent is None:
             self.dir = {"N": (-1, 0), "E": (0, 1), "S": (1, 0), "W": (0, -1)}
             self.agents = {}  # hashtable
             self.goals = {}  # hashtable
             self.boxes = {}  # hashtable
+            self.prevState = None
+            self.prevAction = None
+            self.g = 0
         else:
             self.dir = parent.dir  # rigid
             self.goals = parent.goals  # rigid
             self.agents = copy.deepcopy(parent.agents)
             self.boxes = copy.deepcopy(parent.boxes)
             self.map = copy.deepcopy(parent.map)
-
+            self.prevState = parent  # reference to previous state
+            self.prevAction = None
+            self.g = copy.deepcopy(parent.g) + 1
+        
         super().__init__()
+
+    def getState(self):
+        return {self.agents, self.boxes}
 
     def addMap(self, map2):
         self.map = np.array(map2)
 
-    def addAgent(self, key, value):
+    def addAgent(self, key, value, color='c'):
         # key is color
-        if key not in self.agents:
-            self.agents[key] = [value]
-        else:
-            self.agents[key].append(value)
+        self.map[value] = key
+        self.agents[key] = [[value, color]]
 
     def addGoal(self, key, value):
         # key is letter
         if key not in self.goals:
-            self.goals[key] = [value]
+            self.goals[key] = [[value]]
         else:
-            self.goals[key].append(value)
+            self.goals[key].append([value])
 
-    def addBox(self, key1, key2, value):
+    def addBox(self, key, value, color='c'):
         # key is (letter, color)
-        key = (key1, key2)
+        self.map[value] = key
         if key not in self.boxes:
-            self.boxes[key] = [value]
+            self.boxes[key] = [[value, color]]
         else:
-            self.boxes[key].append(value)
+            self.boxes[key].append([value, color])
 
-    def GetPos(self, objtype, obj, i=0):
+    def getPos(self, objtype, obj, i=0):
         if obj in objtype:
-            return objtype[obj][i]
+            return objtype[obj][i][0]
         else:
             return None
 
-    def SetPos(self, objtype, obj, pos, i=0):
-        if type(objtype[obj][i]) == tuple:
-            objtype[obj][i] = pos
+    def setPos(self, objtype, obj, pos, i=0):
+        if type(objtype[obj][i][0]) == tuple:
+            objtype[obj][i][0] = pos
         else:
             return None
 
@@ -72,116 +79,165 @@ class Literals():
         else:
             return False
 
-    def Move(self):
-        pass
-
-    def Push(self):
-        pass
-
-    def Pull(self):
-        pass
-
-    def Noop(self):
-        pass
-
 
 class StateInit(Literals):
-    def __init__(self, parent: 'Literals' = None):
+    def __init__(self, parent: "Literals" = None):
         # it is (row, column) and not (x, y)
         super().__init__(parent)
 
-    def AddPos(self, agtfrom, agtdir, i=0):
+    def __AddPos(self, agtfrom, agtdir):
         return tuple(map(operator.add, agtfrom, self.dir[agtdir]))
 
-    def Move(self, agt, agtdir):
-        agtfrom = self.GetPos(self.agents, agt)
+    def __MovePrec(self, agt, agtdir):
+        agtfrom = self.getPos(self.agents, agt)
         if agtfrom is None:
             print("agent", agt, "does not exist")
             return None
         if agtdir not in self.dir:
             print("Direction", agtdir, "does not exist")
             return None
-        agtto = self.AddPos(agtfrom, agtdir)
+        agtto = self.__AddPos(agtfrom, agtdir)
         if self.Free(agtto):
-            self.SetPos(self.agents, agt, agtto)
-            # maybe add it to the map and remove the old one?
-            # protype
-            self.map[agtfrom] = chr(32)
-            self.map[agtto] = agt
-            # protype end
-
-            print("Agent " + agt + " is now at " + str(agtto) + " (row,col)")
-            return True
+            return (agt, agtfrom, agtto)
         else:
             print("Pos " + str(agtto) + " (row,col) is not free")
             return None
 
-    def Push(self, agt, boxid, boxcolor, boxdir, i=0):
-        box = (boxid, boxcolor)
-        agtfrom = self.GetPos(self.agents, agt)
-        boxfrom = self.GetPos(self.boxes, box, i)
+    def __MoveEffect(self, agt, agtfrom, agtto):
+        self.setPos(self.agents, agt, agtto)
+        self.map[agtfrom] = chr(32)
+        self.map[agtto] = agt
+        # print("Agent " + agt + " is now at " + str(agtto) + " (row,col)")
+        return True
+
+    def Move(self, agt, agtdir):
+        actionParams = self.__MovePrec(agt, agtdir)
+        if actionParams is not None:
+            return self.__MoveEffect(*actionParams)
+        else:
+            return None
+
+    def __PushPrec(self, agt, boxkey, boxdir, i=0):
+        agtfrom = self.getPos(self.agents, agt)
+        boxfrom = self.getPos(self.boxes, boxkey, i)
         if agtfrom is None:
             print("agent", agt, "does not exist")
             return None
         if boxfrom is None:
-            print("Box", box, "does not exist")
+            print("Box", boxkey, "does not exist")
             return None
         if boxdir not in self.dir:
             print("Direction", boxdir, "does not exist")
             return None
         if self.Neighbour(agtfrom, boxfrom) != 1:
-            print("agent", agt, "and box", box, "are not neighbors")
+            print("agent", agt, "and box", boxkey, "are not neighbors")
             return None
 
-        boxto = self.AddPos(boxfrom, boxdir, i)
+        boxto = self.__AddPos(boxfrom, boxdir)
         if self.Free(boxto):
-            self.SetPos(self.agents, agt, boxfrom, i)
-            self.SetPos(self.boxes, box, boxto, i)
-            # maybe add it to the map and remove the old one?
-            # protype
-            self.map[agtfrom] = chr(32)
-            self.map[boxfrom] = agt
-            self.map[boxto] = boxid
-            # protype end
-
-            print("Agent " + agt + " is now at " + str(boxto) + " (row,col)")
-            print("Box " + str(box) + " is now at " + str(boxfrom) + " (row,col)")
-            return True
+            return (agt, boxkey, agtfrom, boxfrom, boxto, i)
         else:
             print("Pos " + str(boxto) + " (row,col) is not free")
             return None
 
-    def Pull(self, agt, boxid, boxcolor, agtdir, i=0):
-        box = (boxid, boxcolor)
-        agtfrom = self.GetPos(self.agents, agt)
-        boxfrom = self.GetPos(self.boxes, box, i)
+    def __PushEffect(self, agt, boxkey, agtfrom, boxfrom, boxto, i):
+        self.setPos(self.agents, agt, boxfrom, 0)  # agents are unique thus 0
+        self.setPos(self.boxes, boxkey, boxto, i)
+        self.map[agtfrom] = chr(32)
+        self.map[boxfrom] = agt
+        self.map[boxto] = boxkey
+        # print("Agent " + agt + " is now at " + str(boxto) + " (row,col)")
+        # print("Box " + str(box) + " is now at " + str(boxfrom) + " (row,col)")
+        return True
+
+    def Push(self, agt, boxkey, boxdir, i):
+        actionParams = self.__PushPrec(agt, boxkey, boxdir, i)
+        if actionParams is not None:
+            return self.__PushEffect(*actionParams)
+        else:
+            return None
+
+    def __PullPrec(self, agt, boxkey, agtdir, i=0):
+        agtfrom = self.getPos(self.agents, agt)
+        boxfrom = self.getPos(self.boxes, boxkey, i)
         if agtfrom is None:
             print("agent", agt, "does not exist")
             return None
         if boxfrom is None:
-            print("Box", box, "does not exist")
+            print("Box", boxkey, "does not exist")
             return None
         if agtdir not in self.dir:
             print("Direction", agtdir, "does not exist")
             return None
         if self.Neighbour(agtfrom, boxfrom) != 1:
-            print("agent", agt, "and box", box, "are not neighbors")
+            print("agent", agt, "and box", boxkey, "are not neighbors")
             return None
 
-        agtto = self.AddPos(agtfrom, agtdir, i)
+        agtto = self.__AddPos(agtfrom, agtdir)
         if self.Free(agtto):
-            self.SetPos(self.agents, agt, agtto, i)
-            self.SetPos(self.boxes, box, agtfrom, i)
-            # maybe add it to the map and remove the old one?
-            # protype
-            self.map[boxfrom] = chr(32)
-            self.map[agtfrom] = boxid
-            self.map[agtto] = agt
-            # protype end
-
-            print("Agent " + agt + " is now at " + str(agtto) + " (row,col)")
-            print("Box " + str(box) + " is now at " + str(agtfrom) + " (row,col)")
-            return True
+            return (agt, boxkey, agtfrom, agtto, boxfrom, i)
         else:
             print("Pos " + str(agtto) + " (row,col) is not free")
             return None
+
+    def __PullEffect(self, agt, boxkey, agtfrom, agtto, boxfrom, i):
+        self.setPos(self.agents, agt, agtto, 0)  # agents are unique thus 0
+        self.setPos(self.boxes, boxkey, agtfrom, i)
+        self.map[boxfrom] = chr(32)
+        self.map[agtfrom] = boxkey
+        self.map[agtto] = agt
+        # print("Agent " + agt + " is now at " + str(agtto) + " (row,col)")
+        # print("Box " + str(box) + " is now at " + str(agtfrom) + " (row,col)")
+        return True
+    
+    def Pull(self, agt, boxkey, boxdir, i):
+        actionParams = self.__PullPrec(agt, boxkey, boxdir, i)
+        if actionParams is not None:
+            return self.__PullEffect(*actionParams)
+        else:
+            return None
+
+    def get_children(self):
+        children = []
+
+        for direction in self.dir:
+            for agtkey in self.agents:
+
+                actionParams = self.__MovePrec(agtkey, direction)
+                if actionParams is not None:
+                    child = StateInit(self)
+                    child.prevAction = ['Move', actionParams]
+                    child.__MoveEffect(*actionParams)
+                    children.append(child)
+                 
+                # TODO reformat these nested loops and if statements!
+
+                # This can be perhaps be optimized by only looking at boxes at the
+                # neighboring tiles of the agent
+                for boxkey in self.boxes:
+                    for i in range(len(self.boxes[boxkey])):
+                        boxcolor = self.boxes[boxkey][i][1]
+                        
+                        # [agent letter][agent number (0 since it is unique)][color]
+                        if self.agents[agtkey][0][1] == boxcolor:
+                            actionParams = self.__PullPrec(agtkey, boxkey, direction, i)
+                            if actionParams is not None:
+                                child = StateInit(self)
+                                child.prevAction = ['Pull', actionParams]
+                                child.__PullEffect(*actionParams)
+                                children.append(child)
+
+                            actionParams = self.__PushPrec(agtkey, boxkey, direction, i)
+                            if actionParams is not None:
+                                child = StateInit(self)
+                                child.prevAction = ['Push', actionParams]
+                                child.__PushEffect(*actionParams)
+                                children.append(child)
+        
+        for agtkey in self.agents:
+            # TODO make a noop function
+            child = StateInit(self)
+            child.prevAction = ['Noop', None]
+            children.append(child)
+        
+        return children
