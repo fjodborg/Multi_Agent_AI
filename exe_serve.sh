dc94697f61da29ea4c6d2b37a0d478b4c105299d
carrascomj <carrascomurielj@gmail.com>
2020-04-15 21:18:43 +0200

    chore: add CLI handy script

---
+++ exe_serve.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script to run the server
+
+# abs path to directory of repository
+SERVER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [ -z "$1" ];then
+  echo -e "You must supply a level\n" && exit 1
+else
+  lvl=$1
+fi
+
+
+[ -z "$2" ] || method=$2
+mem=2048
+[ -z "$3" ] || mem=$3
+
+echo "Memory allocated: $mem"
+
+java -jar "$SERVER/server.jar" -l "$SERVER/levels/$lvl" -c "python multi_sokoban/searchclient.py $method --max-memory $mem" -g 150 -t 300
