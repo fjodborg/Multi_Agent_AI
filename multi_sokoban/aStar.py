df332a8338030e7a0741212a5a47e4ed84b8e650
carrascomj <carrascomurielj@gmail.com>
2020-04-15 10:41:43 +0200

    fix: clean git related lines

--- multi_sokoban/aStar.py
+++ multi_sokoban/aStar.py
@@ -12,46 +12,38 @@ from searchclient_python import action, heuristic
 from tests import action_example
 from test import test_userlist
 
+
 class PriorityQueue:
     def __init__(self):
         self.elements = []
- 
+
     def empty(self):
         return len(self.elements) == 0
- 
+
     def put(self, item, priority):
         heapq.heappush(self.elements, (priority, item))
-<<<<<<< HEAD
- 
-    #this priority queue returns the lowest value first by using heapq.
-=======
 
     # this priority queue returns the lowest value first by using heapq.
->>>>>>> branch 'master' of https://github.com/FjodBorg/Multi_Agent_AI.git
+
+    # this priority queue returns the lowest value first by using heapq.
     def get(self):
         return heapq.heappop(self.elements)[1]
-<<<<<<< HEAD
-     
-    #heuristic function that tells us how close we are to the goal, by calculating the cost.
-=======
 
     # heuristic function that tells us how close we are to the goal, by calculating the cost.
->>>>>>> branch 'master' of https://github.com/FjodBorg/Multi_Agent_AI.git
     def heuristic(self, a, b):
         (x1, y1) = a
         (x2, y2) = b
         return abs(x1 - x2) + abs(y1 - y2)
-            
 
-<<<<<<< HEAD
-    #The A* algorithm uses the actual distance from the start (costThisfar) and the estimated distance to the goal (spaceSearchCost).
+    # The A* algorithm uses the actual distance from the start (costThisfar) and the estimated distance to the goal (spaceSearchCost).
+
+
 def aStarSearch(startState):
     count = 0  # creating an id for every child
-    
+
     frontier = PriorityQueue()
-    
+
     startPos = actions.StateInit()
-=======
     # The A* algorithm uses the actual distance from the start (costThisfar) and the estimated distance to the goal (spaceSearchCost).
     def aStarSearch(self, searchSpace, start, goal):
         count = 0  # creating an id for every child
@@ -61,96 +53,80 @@ def aStarSearch(startState):
         costThisFar = {}
         cameFrom[start] = None
         costThisFar[start] = 0
->>>>>>> branch 'master' of https://github.com/FjodBorg/Multi_Agent_AI.git
 
-<<<<<<< HEAD
-    frontier.put(startPos, getBestState(startState) , count)
-    
-#     cameFrom = {} # cameFrom keeps track of visited nodes.
-#     costThisFar = {}
-#     cameFrom[start] = None
-#     costThisFar[start] = 0
-    
-#    frontier = initialState #(Given by function call)
-# while/for loop:
-#   leaf = getBestState(frontier) 
-#   if leaf.IsGoalState():
-#      return leaf.bestPath()
-#   explored_states = leaf.explore()
-#   frontier.add(explored_states)
-# getBestState(frontier) 
-#   for all elements in frontier:
-#      pos of box - pos of goals
-#   return shortest distance
+    frontier.put(startPos, getBestState(startState), count)
+
+    #     cameFrom = {} # cameFrom keeps track of visited nodes.
+    #     costThisFar = {}
+    #     cameFrom[start] = None
+    #     costThisFar[start] = 0
+
+    #    frontier = initialState #(Given by function call)
+    # while/for loop:
+    #   leaf = getBestState(frontier)
+    #   if leaf.IsGoalState():
+    #      return leaf.bestPath()
+    #   explored_states = leaf.explore()
+    #   frontier.add(explored_states)
+    # getBestState(frontier)
+    #   for all elements in frontier:
+    #      pos of box - pos of goals
+    #   return shortest distance
 
     while not frontier.empty():
-        count+=1
+        count += 1
         leaf = getBestState(frontier)
-        
-        #current = frontier.get()
-        
+
+        # current = frontier.get()
+
         # Loop ends if goal is found.
         if leaf.isGoalState():
             return leaf.bestPath()
 
-        
         explored_states = leaf.explor()
         frontier.put(explored_states)
 
-#         for elements in frontier:
-#           
-#            # newCost = costThisFar[current] + searchSpace.cost(current, next)
-#             
-#             if elements not in costThisFar or newCost < costThisFar[next]:
-#                 costThisFar[next] = newCost
-#                 newCost + heuristics(goal, next) #priority = actions.explore() #
-#                 frontier.put(next, priority)
-#                 cameFrom[next] = current
+    #         for elements in frontier:
+    #
+    #            # newCost = costThisFar[current] + searchSpace.cost(current, next)
+    #
+    #             if elements not in costThisFar or newCost < costThisFar[next]:
+    #                 costThisFar[next] = newCost
+    #                 newCost + heuristics(goal, next) #priority = actions.explore() #
+    #                 frontier.put(next, priority)
+    #                 cameFrom[next] = current
 
-    return 
+    return
+
 
 def getBestState(frontier):
     i = -1
     hMap = {}
     for element in frontier:
-        i+=1
-        if element.h is None: 
+        i += 1
+        if element.h is None:
             continue
-        
+
         keys = element.getGoals()
         for key in keys:
             goalPositions = element.getGoalsByKey(key)
             boxParams = element.getBoxesByKey(key)
-            
+
             for goalPos in goalPositions:
-                for boxPos,color in boxParams:
-                    
+                for boxPos, color in boxParams:
+
                     boxPos = element.getAgentByKey(key)
-                    goalPos = frontier.getBoxByKey(key)  
-                    # storing the distances between boxes and goals for every state                   
-                    hMap[i] = heuristic(boxPos, goalPos) 
-        #Returns the shortest distance by sorting the smallest value by key
-        element.h = min(hMap, k = hMap.get())
+                    goalPos = frontier.getBoxByKey(key)
+                    # storing the distances between boxes and goals for every state
+                    hMap[i] = heuristic(boxPos, goalPos)
+        # Returns the shortest distance by sorting the smallest value by key
+        element.h = min(hMap, k=hMap.get())
         return element.h
-        
-=======
+
+
 #         while not frontier.empty():
 #             current = frontier.get()
-# 
+#
 #             # Look ends if goal is found.
 #             if current == goal:
 #                 break
->>>>>>> branch 'master' of https://github.com/FjodBorg/Multi_Agent_AI.git
-
-
-
-
-
-
-
-
-
-
-
-
-
