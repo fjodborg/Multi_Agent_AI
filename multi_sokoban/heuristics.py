53109ede71b9400462655f918c43188fbd91d9cc
FjodBorg <Fjordormaster@hotmail.com>
2020-05-13 20:44:22 +0200

    initial changes

--- multi_sokoban/heuristics.py
+++ multi_sokoban/heuristics.py
@@ -143,6 +143,99 @@ class L1Clarkson(Heuristics):
 
     def __init__(self, state: np.array, weight: int = 1):
         """Initialize object by building the VIS(V,E) graph."""
+        self.graph = self.build_graph_discrete(state.map)
+        self.weight = weight
+
+    def build_graph(self, map: np.array) -> List:
+        """Build VIS graph."""
+        rows, cols = map.shape
+        polys = []
+        for row in range(1, rows):
+            lining = False
+            for col in range(1, cols-1):
+                if map[row, col] == "+" and not lining:
+                    lining = True
+                    left = vg.Point(row+1, col+1)
+                elif map[row, col] == " " and lining:
+                    lining = False
+                    polys.append([left, vg.Point(row, col-2)])
+                elif map[row, col] == "+" and col == cols-2 and lining:
+                    lining = False
+                    polys.append([left, vg.Point(row+1, col+1)])
+        g = vg.VisGraph()
+        g.build(polys)
+        return g
+
+    def distance(self, pos1: List, pos2: List) -> List:
+        """Distance from vertice to vertice."""
+        dist = 0
+        path = self.graph.shortest_path(vg.Point(*pos1), vg.Point(*pos2))
+        for p in range(1, len(path)):
+            this = path[p]
+            prev = path[p-1]
+            dist += manha_dist((this.x, this.y), (prev.x, prev.y))
+        return dist
+
+    def __call__(self, states: List):
+        """Calculate heuristic for states in place."""
+        if type(states) is not list:
+            states = [states]
+        if len(states) == 0:
+            return None
+
+        for state in states:
+            box_goal_cost = 0
+            agt_box_cost = 0
+            agt_box_costs = []
+            for key in state.getGoalKeys():
+                goal_params = state.getGoalsByKey(key)
+                box_params = state.getBoxesByKey(key)
+                # maybe add some temporary costs here for each key
+
+                # find every position of goals and boxes with the given key
+                for goal_pos, goal_color in goal_params:
+                    box_goal_costs = []
+                    for box_pos, _ in box_params:
+                        # only take agents with the same color as goalColor
+                        agent_keys = state.getAgentsByColor(goal_color)
+
+                        if manha_dist(goal_pos, box_pos) == 0:
+                            continue
+
+                        for agent_key in agent_keys:
+                            agentPos = state.getAgentsByKey(agent_key)[0][0]
+                            agt_box_costs.append(self.distance(agentPos, box_pos))
+
+                        box_goal_costs.append(self.distance(box_pos, goal_pos))
+
+                    if len(box_goal_costs) > 0:
+                        box_cost = min(box_goal_costs)
+                        if key.lower() == self.weight:
+                            box_cost *= 10
+                        box_goal_cost += box_cost
+                if len(agt_box_costs) > 0:
+                    agt_box_cost += sum(agt_box_costs)
+
+            state.h = box_goal_cost + agt_box_cost
+            state.f = state.h * 5 + state.g
+
+
+class dGraph(Heuristics):
+    """L1 path finding heuristics.
+
+    Outside corners are calculated and a visual graph is built where the edges
+    are the manhattan distance from visual corner to visual corner.
+
+    The algorithm computes the distance to the object to the min visual corner
+    and then use the distances in the graph to calculate the heuristic.
+
+    Reference
+    ---------
+    https://dl.acm.org/doi/10.1145/41958.41985
+    """
+
+    def __init__(self, state: np.array, weight: int = 1):
+        """Initialize object by building the VIS(V,E) graph."""
         self.graph = self.build_graph(state.map)
         self.weight = weight
 
